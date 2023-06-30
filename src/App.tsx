03eb34a23f90951912207a9f957a36cc65ad20d7
IGOR_EIDELMAN <v1148656@gmail.com>
2023-06-30 17:13:32 +0300

    Secon commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="/react_redux_pizza" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/search" element={<Search />} />
           <Route path="/pizza/:id" element={<Pizza />} />
